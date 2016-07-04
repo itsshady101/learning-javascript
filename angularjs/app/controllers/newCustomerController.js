690ccecab32f43159c40dcab0f781e5a5e8212f0
itsshady101 <rocks.ayush2011@gmail.com>
2016-07-04 21:53:26 +0530

    Ng-src

--- angularjs/app/controllers/newCustomerController.js
+++ angularjs/app/controllers/newCustomerController.js
@@ -5,9 +5,9 @@ app.controller('userController' ,function() {
 	this.toggle = false;
 
 	this.stars = [
-		{name: "Rachell", age: 29},
-		{name: "Alex", age: 25},
-		{name: "Ayush", age: 16}
+		{name: "Rachell", age: 29, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg'},
+		{name: "Alex", age: 25, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/evagiselle/128.jpg'},
+		{name: "Ayush", age: 16, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/_everaldo/128.jpg'}
 	];
 
 	this.form = function() {
